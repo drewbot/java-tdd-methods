a785e4f91157d7eb0733ad842209562b5ce0bdf2
Drew Botka <drewbotka@gmail.com>
2017-09-14 11:07:11 -0400

    test driven division

---
+++ src/test/java/io/drewbot/SanityTest.java
@@ -0,0 +1,13 @@
+package io.drewbot;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SanityTest {
+
+    @Test
+    public void OurSanityTest() throws Exception {
+        assertEquals(3, 3);
+    }
+}
